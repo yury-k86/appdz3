49c594ae43ecdb1ec1f10cbe3fb8c382718e18ba
Yury <y.kostyuchenko@gmail.com>
2013-04-23 03:38:11 -0700

    Commiting features_1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,10 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(:title => movie[:title], :release_date => movie[:release_date], :rating => movie[:rating])
   end
-  flunk "Unimplemented"
+  #return @movies
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +16,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  #puts page.body.index('<td>' + e1 + '</td>')
+  #puts page.body.index('<td>' + e2 + '</td>')
+  assert page.body.index('<td>' + e1 + '</td>') < page.body.index('<td>' + e2 + '</td>')
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +29,35 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(',').each do |rating|
+    if uncheck == "un" then
+       uncheck('ratings_' + rating)
+    else
+       check('ratings_' + rating)
+    end
+  end
+end
+
+When /(no|all) ratings selected/ do |option|
+  #puts option
+  Movie.all_ratings.each do |rating|
+     if option == 'no' then
+        #puts 'true'
+	uncheck('ratings_' + rating)
+     else
+	check('ratings_' + rating)
+     end
+  end
+end
+
+Then /I should see (no|all) of the movies/ do |option|
+  if option == 'no' then
+    movies_count = 0
+  else
+    movies_count = Movie.count()
+  end
+  rows = find("table#movies").all('tr')
+  assert (rows.length - 1) == movies_count
+#print page.find_by_id('movies').has_table?
+#assert table[:movies].rows == movies_count
 end
